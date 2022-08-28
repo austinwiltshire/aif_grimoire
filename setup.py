e1c7596728403c81ebbd83c880a2123056eecdf6
John Graham <john.a.graham@gmail.com>
2022-08-28 15:37:59 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aif_grimoire',
+    version='0.1',
+    packages=['aif_grimoire'],
+    url='',
+    license='MIT',
+    author='john',
+    author_email='john.a.graham@gmail.com',
+    description='Helpers for American Iron Giant'
+)
